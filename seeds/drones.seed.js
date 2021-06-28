313fb8678e930a6545a6516aa9952af3d7156a07
laurindor <arual.socram@gmail.com>
2021-06-28 18:15:00 +0200

    it1 done

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,25 @@
 // Iteration #1
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model');
+
+//connecting database
+
+//const DB_NAME = 'lab-express-drones'
+//mongoose.connect(`mongodb://localhost/${dbName}`)
+
+//require database config
+
+require('../db/index.js');
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+
+ Drone.insertMany( drones)
+ .then((result) => console.log(`drone added: ${result.name}`))
+ .catch((err) => console.log(err));
+
+   
